21e3a50ecff89feab85fe7f36f37f868d63c5558
passhuman <ctctct801@gmail.com>
2024-01-08 11:39:31 +0900

    DAY1_PROJECT

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace helloworld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string man = "아저씨";
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(man);
+    }
+}
